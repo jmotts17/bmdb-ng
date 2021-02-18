816affd5922b80163a99f83c01a846252c6140d6
Josh Motta <joshua.a.motta@gmail.com>
2021-02-17 19:00:13 -0500

    Added sorting to actor list

--- src/app/feature/actor/actor-list/actor-list.component.ts
+++ src/app/feature/actor/actor-list/actor-list.component.ts
@@ -12,6 +12,9 @@ export class ActorListComponent implements OnInit {
 
   title = "Actor List";
   actors: Actor[] = [];
+  sortCriteria: string = "id";
+  sortOrder: string = "asc";
+  colClasses = "btn btn-link font-weight-bold";
 
   constructor(private actorSvc: ActorService,
               private sysSvc: SystemService) { }
@@ -29,6 +32,14 @@ export class ActorListComponent implements OnInit {
         console.log(err);
       }
     )
+  }
+
+  sortBy(column: string): void {
+    console.log("movie list sortBy called")
+    if(column == this.sortCriteria){
+      this.sortOrder = (this.sortOrder == "desc") ? "asc" : "desc";
+    }
+    this.sortCriteria = column;
   }
 
 }
